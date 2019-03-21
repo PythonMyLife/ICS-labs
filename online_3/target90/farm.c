dc6ba537fcf0c6c4ffb80febadcac511e8ff5f50
PythonMyLife <1425439522@qq.com>
2019-03-21 22:54:26 +0800

    files of online test

---
+++ online_3/target90/farm.c
@@ -0,0 +1,223 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned addval_359(unsigned x)
+{
+    return x + 3281017013U;
+}
+
+void setval_127(unsigned *p)
+{
+    *p = 2462550344U;
+}
+
+void setval_146(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+unsigned addval_406(unsigned x)
+{
+    return x + 2462550344U;
+}
+
+void setval_212(unsigned *p)
+{
+    *p = 3281031256U;
+}
+
+unsigned getval_186()
+{
+    return 3347662908U;
+}
+
+unsigned addval_183(unsigned x)
+{
+    return x + 549935225U;
+}
+
+unsigned addval_360(unsigned x)
+{
+    return x + 2425393232U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned addval_275(unsigned x)
+{
+    return x + 3269495112U;
+}
+
+void setval_261(unsigned *p)
+{
+    *p = 3281047177U;
+}
+
+void setval_391(unsigned *p)
+{
+    *p = 3234120329U;
+}
+
+unsigned getval_161()
+{
+    return 3281044105U;
+}
+
+unsigned addval_482(unsigned x)
+{
+    return x + 2425406088U;
+}
+
+void setval_481(unsigned *p)
+{
+    *p = 3353381192U;
+}
+
+unsigned getval_286()
+{
+    return 3677930113U;
+}
+
+unsigned addval_462(unsigned x)
+{
+    return x + 2463205771U;
+}
+
+unsigned addval_123(unsigned x)
+{
+    return x + 3376991881U;
+}
+
+unsigned addval_204(unsigned x)
+{
+    return x + 3223372425U;
+}
+
+unsigned getval_211()
+{
+    return 3286272328U;
+}
+
+unsigned getval_113()
+{
+    return 3224424073U;
+}
+
+unsigned addval_270(unsigned x)
+{
+    return x + 3286272072U;
+}
+
+unsigned getval_304()
+{
+    return 3264270729U;
+}
+
+unsigned addval_383(unsigned x)
+{
+    return x + 3380924808U;
+}
+
+void setval_490(unsigned *p)
+{
+    *p = 3531915785U;
+}
+
+void setval_300(unsigned *p)
+{
+    *p = 3524841865U;
+}
+
+unsigned getval_158()
+{
+    return 3353381192U;
+}
+
+unsigned addval_493(unsigned x)
+{
+    return x + 2464188744U;
+}
+
+void setval_392(unsigned *p)
+{
+    *p = 3526939017U;
+}
+
+unsigned getval_308()
+{
+    return 3281047961U;
+}
+
+unsigned getval_310()
+{
+    return 3223372417U;
+}
+
+void setval_199(unsigned *p)
+{
+    *p = 3221802633U;
+}
+
+unsigned getval_224()
+{
+    return 2496760206U;
+}
+
+void setval_172(unsigned *p)
+{
+    *p = 3352201654U;
+}
+
+unsigned addval_235(unsigned x)
+{
+    return x + 3466191887U;
+}
+
+unsigned getval_365()
+{
+    return 2497743176U;
+}
+
+unsigned getval_181()
+{
+    return 3286272328U;
+}
+
+unsigned addval_122(unsigned x)
+{
+    return x + 3285093239U;
+}
+
+unsigned getval_247()
+{
+    return 3682910856U;
+}
+
+void setval_426(unsigned *p)
+{
+    *p = 3515439952U;
+}
+
+void setval_215(unsigned *p)
+{
+    *p = 3531918987U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
---
+++ online_3/target90/p12_code.s
@@ -0,0 +1,3 @@
+mov  $0x4b7a4937,%rdi
+push $0x401830
+ret
